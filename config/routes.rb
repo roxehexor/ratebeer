f56d8b5f0a23a19ad24ef396f025b3c9feb4901a
roxehexor <aripekka.perhoniemi@gmail.com>
2017-01-24 20:53:03 +0200

    navibar added and homepage changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'breweries#index'
+  
   resources :beers
   resources :breweries
   # The priority is based upon order of creation: first created -> highest priority.
